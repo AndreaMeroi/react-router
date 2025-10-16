e58a8ee07496ae9b45a742291569be1cc694fbba
AndreaMeroi <andreameroidublin@gmail.com>
2025-10-16 16:22:21 +0200

    add last

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,39 @@
+import { Link } from "react-router-dom"
+
 export default function AppFooter() {
 
     return (
-        <footer>Copyright 2025</footer>
+        <footer className="bg-black text-white">
+            <div container-fluid>
+                <div className="row">
+
+                    <div col-12 col-sm-6 col-md-4>
+                        <h3>Quick links</h3>
+                        <ul className="list-unstyled">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/ChiSiamo">Chi Siamo</Link></li>
+                            <li><Link to="/Prodotti">Prodotti</Link></li>
+                        </ul>
+                    </div>
+                    <div col-12 col-sm-6 col-md-4>
+                        <h3>Cookies</h3>
+                        <ul className="list-unstyled">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/ChiSiamo">Chi Siamo</Link></li>
+                            <li><Link to="/Prodotti">Prodotti</Link></li>
+                        </ul>
+                    </div>
+                    <div col-12 col-sm-6 col-md-4>
+                        <h3>Policy</h3>
+                        <ul className="list-unstyled">
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/ChiSiamo">Chi Siamo</Link></li>
+                            <li><Link to="/Prodotti">Prodotti</Link></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
     )
 }
--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -3,12 +3,57 @@ import AppFooter from "../components/AppFooter"
 
 export default function Prodtti() {
 
+
+
     return (
 
         <>
             <AppHeader />
 
-            <main>ProdottiProdottiProdottiProdottiProdottiProdottiProdottiProdottiProdottiProdottiProdotti</main>
+            <main>
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-md-2 row cols-lg-3 g-3">
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <img className="card-img-top" src="https://picsum.photos/seed/picsum/200/300" alt="" />
+                            <div card-body>
+                                <h3>TEXT</h3>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </main>
 
             <AppFooter />
         </>
